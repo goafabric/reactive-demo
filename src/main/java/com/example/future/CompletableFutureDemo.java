dedde12b47611647d30d359d95032f5f636cc845
Mautsch, Andreas <andreas.mautsch@cgm.com>
2018-06-18 16:22:00 +0200

    functions removed

--- src/main/java/com/example/future/CompletableFutureDemo.java
+++ src/main/java/com/example/future/CompletableFutureDemo.java
@@ -5,11 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.function.Function;
-import java.util.function.Supplier;
 
 @Slf4j
 public class CompletableFutureDemo {
+    /*
     private static class MySupplier implements Supplier<Integer> {
 
         @Override
@@ -23,9 +22,6 @@ public class CompletableFutureDemo {
         }
     }
 
-    /**
-     * A (pure) function that adds one to a given Integer
-     **/
     private static class PlusOne implements Function<Integer, Integer> {
 
         @Override
@@ -33,11 +29,18 @@ public class CompletableFutureDemo {
             return x + 1;
         }
     }
+    */
 
     public static void main(String[] args) throws Exception {
         ExecutorService exec = Executors.newSingleThreadExecutor();
-        CompletableFuture<Integer> f = CompletableFuture.supplyAsync(new MySupplier(), exec);
-        CompletableFuture<Integer> f2 = f.thenApply(new PlusOne());
+        CompletableFuture<Integer> f = CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+            }
+            return 1;
+        }, exec);
+        CompletableFuture<Integer> f2 = f.thenApply(x -> x + 1);
 
         log.info("" + f.isDone()); // False
         log.info("" + f.get()); // Waits until the "calculation" is done, then prints 2
